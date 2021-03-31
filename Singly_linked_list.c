85268d2b3f7aa092b0cf90ebe5806b79c9d08832
Dipan Nama <dipannama91@gmail.com>
2021-03-31 14:34:11 +0530

    Create Singly_linked_list.c

---
+++ Singly_linked_list.c
@@ -0,0 +1,68 @@
+#include<stdlib.h> 
+#include <stdio.h>
+
+void create();
+void display();
+void insert_begin();
+void insert_end();
+void insert_pos();
+void delete_begin();
+void delete_end();
+void delete_pos();
+
+struct node
+{
+int info;
+struct node *next;
+}
+
+struct node *start = NULL; 
+
+int main()
+{
+int choice;
+while (1) {
+
+printf("\n MENU \n");
+
+printf("\n 1.Create \n);
+
+printf("\n 2.Display \n");
+
+printf("\n 3.Insert at the beginning\n");
+
+printf("\n 4.Insert at the end \n"):
+
+print ("\n 5.Innert at specified position printf(\n 6. Delete from beginning ")
+
+printf("\n 7. Delete from the end
+
+printf ("\n 8.Delete from specified position
+
+printf ("\n 9.Exit
+
+print
+
+printti Enter your choicert")
+
+scant (d, schoice)
+
+switch (choice)
+
+Case 1:
+
+--n")
+
+create (
+
+break;
+
+display it break
+
+insert begin ()a
+
+break:
+
+insert end (1 break
+
+insert pos ( breaks
